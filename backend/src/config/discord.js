89bcb8c272640d7d37896f5faef917d1a326036d
VoidYeezy <156734366+ott0111@users.noreply.github.com>
2026-03-01 03:19:47 -0500

    Update discord.js

--- backend/src/config/discord.js
+++ backend/src/config/discord.js
@@ -1,1 +1,13 @@
+const {Client,GatewayIntentBits}=require("discord.js")
 
+const client=new Client({
+ intents:[GatewayIntentBits.Guilds]
+})
+
+client.once("ready",()=>{
+ console.log("Discord bot ready")
+})
+
+client.login(process.env.DISCORD_BOT_TOKEN)
+
+module.exports={client}
