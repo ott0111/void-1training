b61f9385a0380cb368f8dc975c8130dc82248328
VoidYeezy <156734366+ott0111@users.noreply.github.com>
2026-03-01 03:35:29 -0500

    Update logger.js

--- backend/src/utils/logger.js
+++ backend/src/utils/logger.js
@@ -1,1 +1,11 @@
+function time(){
+ return new Date().toISOString()
+}
 
+const logger={
+ info:(...msg)=>console.log(`[${time()}]`,...msg),
+ warn:(...msg)=>console.warn(`[${time()}]`,...msg),
+ error:(...msg)=>console.error(`[${time()}]`,...msg)
+}
+
+module.exports={logger}
